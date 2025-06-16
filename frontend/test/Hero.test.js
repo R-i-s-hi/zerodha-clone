0c3efc2412744b7ab821a826a2394c225371e6eb
Rishabh Singh <tcrr2005@gmail.com>
2025-06-16 13:25:13 +0530

    added testing file

---
+++ frontend/test/Hero.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Hero from '../components/Hero';
+
+// test suite for Hero component(can perform multiple test cases)
+describe('Hero Component(Home)', () => {
+  test('renders Hero component with correct title and subtitle and image', () => {
+
+    render(<Hero/>);
+    const imageElement = screen.getByAltText("hero-image"); // alt text of the image
+    expect(imageElement).toBeInTheDocument(); // check if image is in the document
+    expect(imageElement).toHaveAttribute('src', 'images/homeHero.png'); // check if image source is correct
+  
+  });
+})
